4fa7e92caf34cb4009d2543e2867efe3a8200574
Heran-foo <heranghalley123@gmail.com>
2024-08-06 11:39:26 +0600

    update

--- resources/views/employee/employee-list/create.blade.php
+++ resources/views/employee/employee-list/create.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.app')
 @section('page-title', 'Approval Rules')
 @section('content')
-@php
-    $currentTab = request()->query('employee', 'personal-info');    
-@endphp
+
 <div class="card">
     <div class="card-body">
         <ul class="nav nav-pills mb-4" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button href="{{ route('employee-list.create', ['employee' => 'personal-info']) }}" class="nav-link {{ $currentTab == 'personal-info' ? 'active' : '' }}" id="pills-personal-tab" data-bs-toggle="pill" data-bs-target="#pills-personal" type="button" role="tab" aria-controls="pills-personal" aria-selected="{{ $currentTab == 'personal-info' ? 'true' : 'false' }}">Personal Information</button>
+                <button class="nav-link active" id="pills-personal-tab" data-bs-toggle="pill" data-bs-target="#pills-personal" type="button" role="tab" aria-controls="pills-personal" aria-selected="true">Personal Information</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button href="{{ route('employee-list.create', ['employee' => 'address']) }}" class="nav-link {{ $currentTab == 'address' ? 'active' : '' }}" id="pills-address-tab" data-bs-toggle="pill" data-bs-target="#pills-address" type="button" role="tab" aria-controls="pills-address" aria-selected="{{ $currentTab == 'personal-info' ? 'true' : 'false' }}">Address</button>
+                <button class="nav-link" id="pills-address-tab" data-bs-toggle="pill" data-bs-target="#pills-address" type="button" role="tab" aria-controls="pills-address" aria-selected="false">Address</button>
             </li>  
             <li class="nav-item" role="presentation">
                 <button class="nav-link" id="pills-qualification-tab" data-bs-toggle="pill" data-bs-target="#pills-qualification" type="button" role="tab" aria-controls="pills-qualification" aria-selected="false">Qualification</button>
