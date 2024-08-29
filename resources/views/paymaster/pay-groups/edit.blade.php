83504a0e3a6c6504ff3887c8bbead21e852d0f86
Phuntsho Dema <pempoo1998@gmail.com>
2024-08-29 10:19:05 +0600

    changes in pay head

--- resources/views/paymaster/pay-groups/edit.blade.php
+++ resources/views/paymaster/pay-groups/edit.blade.php
@@ -30,74 +30,7 @@
 <!-- Pay Group Details Form -->
 @include('paymaster.pay-group-details.index', ['payGroup' => $payGroup])
 
-<!-- Edit Modal -->
-<div class="modal fade" id="edit-modal" tabindex="-1" aria-labelledby="editDetailLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content" style="background-color: #f8f9fa;">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editDetailLabel">Edit Detail</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
 
-            <div class="modal-body">
-                <form action="" method="POST" id="edit-modal-form">
-                    @csrf
-                    @method('PUT')
-
-                    <div class="mb-3 dynamic-field" id="field_employee_category">
-                        <label for="employee_category" class="form-label">Employee Category <span class="text-danger">*</span></label>
-                        <select class="form-control" id="employee_category" name="employee_category">
-                            <option value="" disabled selected>Select an option</option>
-                            <option value="1">Critical Staff</option>
-                        </select>
-                    </div>
-
-                    <div class="mb-3 dynamic-field" id="field_grade">
-                        <label for="grade" class="form-label">Grade <span class="text-danger">*</span></label>
-                        <select class="form-control" id="grade" name="grade">
-                            <option value="" disabled selected hidden>Select Grade</option>
-                            @foreach ($grades as $grade)
-                            <option value="{{ $grade->id }}">{{ $grade->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="mb-3 dynamic-field" id="field_calculation_method">
-                        <label for="calculation_method" class="form-label">Calculation Method <span class="text-danger">*</span></label>
-                        <select class="form-control" id="calculation_method" name="calculation_method">
-                            <option value="" disabled selected hidden>Select Calculation Method</option>
-                            @foreach (config('global.calculation_method') as $key => $value)
-                            <option value="{{ $key }}">{{ $value }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="mb-3 dynamic-field" id="field_amount">
-                        <label for="amount" class="form-label">Amount <span class="text-danger">*</span></label>
-                        <input type="number" class="form-control" id="amount" name="amount">
-                    </div>
-
-                    <div class="modal-footer d-flex justify-content-center">
-                        <button type="submit" class="btn btn-primary">Save changes</button>
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<!-- Custom backdrop style -->
-<style>
-    .modal-backdrop {
-        background-color: rgba(255, 255, 255, 0.7) !important;
-    }
-
-    .dynamic-field {
-        display: none;
-    }
-</style>
 
 
 @include('layouts.includes.delete-modal')
