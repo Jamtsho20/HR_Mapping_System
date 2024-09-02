9b19c3d04ea2219c9dd071ee297e72b5e95d053b
Phuntsho Dema <pempoo1998@gmail.com>
2024-09-02 15:11:26 +0600

    pay group edited

--- resources/views/paymaster/pay-groups/edit.blade.php
+++ resources/views/paymaster/pay-groups/edit.blade.php
@@ -14,7 +14,7 @@
             </div>
             <div class="form-group col-md-12">
                 <label for="applicable_on">Applicable On<span class="text-danger">*</span></label>
-                <select name="applicable_on" class="form-control" required>
+                <select name="applicable_on" id="applicable_on" class="form-control" required>
                     <option value="" disabled selected hidden>Select an option</option>
                     <option value="1" {{ old('applicable_on', $payGroup->applicable_on) == 1 ? 'selected' : '' }}>Employee Group</option>
                     <option value="2" {{ old('applicable_on', $payGroup->applicable_on) == 2 ? 'selected' : '' }}>Grade</option>
@@ -30,41 +30,8 @@
 <!-- Pay Group Details Form -->
 @include('paymaster.pay-group-details.index', ['payGroup' => $payGroup])
 
-
-
-
 @include('layouts.includes.delete-modal')
 @endsection
 @push('page_scripts')
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const payGroupNameInput = document.getElementById('pay-group-name');
-        const dynamicFields = document.querySelectorAll('.dynamic-field');
-        const gradeField = document.getElementById('field_grade');
 
-        const toggleFields = (name) => {
-            dynamicFields.forEach(field => {
-                field.style.display = 'none';
-            });
-
-            if (name === 'Critical Staff Group') {
-                document.getElementById('field_employee_category').style.display = 'block';
-                document.getElementById('field_calculation_method').style.display = 'block';
-                document.getElementById('field_amount').style.display = 'block';
-            } else if (name === 'Grade Wise SIFA' || name === 'Grade Wise Communication Allowance') {
-                gradeField.style.display = 'block';
-                document.getElementById('field_calculation_method').style.display = 'block';
-                document.getElementById('field_amount').style.display = 'block';
-            }
-        };
-
-        // Trigger field visibility when the input value changes
-        payGroupNameInput.addEventListener('input', function() {
-            toggleFields(this.value);
-        });
-
-        // Trigger field visibility on page load based on the existing value
-        toggleFields(payGroupNameInput.value);
-    });
-</script>
 @endpush
