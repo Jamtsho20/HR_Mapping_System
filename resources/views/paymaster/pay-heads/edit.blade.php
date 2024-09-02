c89f1ebc4298cf80fc548a841314ca13ab843d31
Phuntsho Dema <pempoo1998@gmail.com>
2024-09-02 10:12:12 +0600

    changed in pay head

--- resources/views/paymaster/pay-heads/edit.blade.php
+++ resources/views/paymaster/pay-heads/edit.blade.php
@@ -36,13 +36,13 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="name">Name <span class="text-danger">*</span></label>
-                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $payHead->name) }}" required>
+                        <input type="text" name="name" id="name" class="form-control" value="{{ $payHead->name }}" required>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="code">Code <span class="text-danger">*</span></label>
-                        <input type="text" name="code" id="code" class="form-control" value="{{ old('code', $payHead->code) }}" required>
+                        <input type="text" name="code" id="code" class="form-control" value="{{ $payHead->code }}" required>
                     </div>
                 </div>
             </div>
@@ -64,7 +64,12 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="form-group" id="calculated_on_wrapper">
+                    <div class="form-group" id="calculated_on_wrapper"
+                        @if(in_array($payHead->calculation_method, [1, 6, 7]))
+                        style="display: none;"
+                        @else
+                        style="display: block;"
+                        @endif>
                         <label for="calculated_on">Calculated On <span class="text-danger">*</span></label>
                         <select name="calculated_on" id="calculated_on" class="form-control">
                             <option value="" disabled>Select your option</option>
@@ -77,155 +82,99 @@
                             <option value="7" {{ $payHead->calculated_on == 7 ? 'selected' : '' }}>By Formula</option>
                         </select>
                     </div>
+
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group" id="pay_slab_wrapper">
-                        <label for="mas_pay_slab_id">Pay Slab <span class="text-danger">*</span></label>
-                        <select name="mas_pay_slab_id" id="mas_pay_slab_id" class="form-control">
-                            <option value="" disabled>Select Pay Slab</option>
-                            @foreach($paySlabs as $paySlab)
-                            <option value="{{ $paySlab->id }}" {{ $payHead->mas_pay_slab_id == $paySlab->id ? 'selected' : '' }}>
-                                {{ $paySlab->name }}
-                            </option>
-                            @endforeach
-                        </select>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group" id="pay_slab_wrapper"
+                            @if($payHead->calculation_method == 3)
+                            style="display: block;"
+                            @else
+                            style="display: none;"
+                            @endif>
+                            <label for="mas_pay_slab_id">Pay Slab <span class="text-danger">*</span></label>
+                            <select name="mas_pay_slab_id" id="mas_pay_slab_id" class="form-control">
+                                <option value="" disabled>Select Pay Slab</option>
+                                @foreach($paySlabs as $paySlab)
+                                <option value="{{ $paySlab->id }}" {{ $payHead->mas_pay_slab_id == $paySlab->id ? 'selected' : '' }}>
+                                    {{ $paySlab->name }}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group" id="pay_group_wrapper"
+                            @if($payHead->calculation_method == 4)
+                            style="display: block;"
+                            @else
+                            style="display: none;"
+                            @endif>
+                            <label for="mas_pay_group_id">Pay Group <span class="text-danger">*</span></label>
+                            <select name="mas_pay_group_id" id="mas_pay_group_id" class="form-control">
+                                <option value="" disabled>Select Pay Group</option>
+                                @foreach($payGroups as $payGroup)
+                                <option value="{{ $payGroup->id }}" {{ $payHead->mas_pay_group_id == $payGroup->id ? 'selected' : '' }}>
+                                    {{ $payGroup->name }}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
 
-                    <div class="form-group" id="pay_group_wrapper">
-                        <label for="mas_pay_group_id">Pay Group <span class="text-danger">*</span></label>
-                        <select name="mas_pay_group_id" id="mas_pay_group_id" class="form-control">
-                            <option value="" disabled>Select Pay Group</option>
-                            @foreach($payGroups as $payGroup)
-                            <option value="{{ $payGroup->id }}" {{ $payHead->mas_pay_group_id == $payGroup->id ? 'selected' : '' }}>
-                                {{ $payGroup->name }}
-                            </option>
-                            @endforeach
-                        </select>
-                    </div>
 
-                    <div class="form-group" id="amount_wrapper">
+                    <div class="form-group" id="amount_wrapper"
+                        @if(in_array($payHead->calculation_method, [1, 2, 5]))
+                        style="display: block;"
+                        @else
+                        style="display: none;"
+                        @endif>
                         <label for="amount">Amount <span class="text-danger">*</span></label>
-                        <input type="number" name="amount" id="amount" class="form-control" value="{{ old('amount', $payHead->amount) }}">
+                        <input type="number" name="amount" id="amount" class="form-control" value="{{ $payHead->amount }}">
                     </div>
 
-                    <div class="form-group" id="formula_wrapper">
+                    <div class="form-group" id="formula_wrapper"
+                        @if($payHead->calculation_method == 6)
+                        style="display: block;"
+                        @else
+                        style="display: none;"
+                        @endif>
                         <label for="formula">Formula <span class="text-danger">*</span></label>
-                        <textarea name="formula" id="formula" class="form-control">{{ old('formula', $payHead->formula) }}</textarea>
+                        <textarea name="formula" id="formula" class="form-control">{{ $payHead->formula }}</textarea>
                     </div>
+
                 </div>
             </div>
 
             <div class="card-footer text-center">
-                <button type="submit" class="btn btn-primary"><i class="fa fa-upload"></i> Update</button>
-                <a href="{{ url('paymaster/pay-heads') }}" class="btn btn-danger"><i class="fa fa-undo"></i> Cancel</a>
+                <button type="submit" class="btn btn-primary"><i class="fa fa-upload"></i> Save</button>
+                <a href="{{ url('paymaster/pay-heads') }}" class="btn btn-danger"><i class="fa fa-undo"></i> CANCEL</a>
             </div>
         </div>
     </div>
 </form>
-<div class="row mt-4">
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <h5><strong>Formula Variables</strong></h5>
-            </div>
-            <div class="card-body">
-                <ul>
-                    <li>BASIC_PAY</li>
-                    <li>NET_PAY</li>
-                    <li>PIT_NET_PAY</li>
-                    <li>GROSS_PAY</li>
-                    <li>PAY_SCALE_BASE_PAY</li>
-                    <li>MONTHS_IN_SERVICE</li>
-                    <li>YEARS_IN_SERVICE</li>
-                    <li>MONTHS_SINCE_REGULARISATION</li>
-                    <li>YEARS_SINCE_REGULARISATION</li>
-                    <li>OVERTIME_HOURS</li>
-                    <li>HOURLY_WAGE</li>
-                    <li>GRADE</li>
-                    <li>GRADE_STEP</li>
-                </ul>
-
-                <p><strong>Conditional Operators:</strong></p>
-                    <ul>
-                        <li>IF</li>
-                        <li>THEN</li>
-                        <li>ELSEIF</li>
-                        <li>ENDIF</li>
-                    </ul>
-
-                <p><strong>Comparison Operators:</strong></p>
-                    <ul>
-                        <li>&gt; (greater than)</li>
-                        <li>&lt; (less than)</li>
-                        <li>&gt;= (greater than or equal to)</li>
-                        <li>&lt;= (less than or equal to)</li>
-                        <li>== (equal to)</li>
-                        <li>!= (not equal to)</li>
-                    </ul>
-
-                <p><strong>Logical Operators:</strong></p>
-                    <ul>
-                        <li>&amp; (AND)</li>
-                        <li>|| (OR)</li>
-                    </ul>
- 
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <h5><strong>Sample Formula</strong></h5>
-            </div>
-            <div class="card-body">
-                <pre>
-                    IF ([BASIC_PAY] &lt; 10000)
-                    THEN (0.10 * [BASIC_PAY])
-                    ELSE
-                    THEN (0.20 * [BASIC_PAY])
-                    ENDIF
-                </pre>
-
-                <p><strong>Note:</strong></p>
-                <ol>
-                    <li>Wrap Variables in Square Brackets - E.g. [BASIC_PAY]</li>
-                    <li>Wrap Expressions in brackets - E.g. (0.3 * [BASIC_PAY])</li>
-                    <li>All IF conditions should have a closing ENDIF</li>
-                    <li>Each computation expression should have a THEN keyword in front. E.g. THEN ([BASIC_PAY]/12)</li>
-                    <li>Each conditional or computation expression should start on a new line</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</div>
-
-@section('scripts')
+@include('paymaster.pay-heads.form')
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function() {
         const calculationMethodSelect = document.getElementById('calculation_method');
+        const amountWrapper = document.getElementById('amount_wrapper');
         const calculatedOnWrapper = document.getElementById('calculated_on_wrapper');
         const paySlabWrapper = document.getElementById('pay_slab_wrapper');
         const payGroupWrapper = document.getElementById('pay_group_wrapper');
-        const amountWrapper = document.getElementById('amount_wrapper');
         const formulaWrapper = document.getElementById('formula_wrapper');
 
-        function updateVisibility() {
-            const selectedMethod = calculationMethodSelect.value;
+        function toggleFields() {
+            const method = parseInt(calculationMethodSelect.value);
 
-            calculatedOnWrapper.style.display = ['1', '6', '7'].includes(selectedMethod) ? 'none' : 'block';
-            paySlabWrapper.style.display = selectedMethod === '3' ? 'block' : 'none';
-            payGroupWrapper.style.display = selectedMethod === '4' ? 'block' : 'none';
-            amountWrapper.style.display = ['1', '2', '5'].includes(selectedMethod) ? 'block' : 'none';
-            formulaWrapper.style.display = selectedMethod === '6' ? 'block' : 'none';
+            amountWrapper.style.display = [1, 2, 5].includes(method) ? 'block' : 'none';
+            calculatedOnWrapper.style.display = [1, 6, 7].includes(method) ? 'none' : 'block';
+            paySlabWrapper.style.display = method === 3 ? 'block' : 'none';
+            payGroupWrapper.style.display = method === 4 ? 'block' : 'none';
+            formulaWrapper.style.display = method === 6 ? 'block' : 'none';
         }
 
-        calculationMethodSelect.addEventListener('change', updateVisibility);
-        updateVisibility(); // Set initial visibility
+        calculationMethodSelect.addEventListener('change', toggleFields);
+        toggleFields();
     });
 </script>
-@endsection
 @endsection
