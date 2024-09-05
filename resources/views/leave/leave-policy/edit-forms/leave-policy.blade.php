8bc055b22e4eb7abf23b0c312c37f9d8c7ab8386
kingaseldon <kingaseldon36@gmail.com>
2024-09-05 14:52:22 +0600

    vh

--- resources/views/leave/leave-policy/edit-forms/leave-policy.blade.php
+++ resources/views/leave/leave-policy/edit-forms/leave-policy.blade.php
@@ -9,7 +9,7 @@
         <select class="form-control" id="ddlLeaveType" name="leave_policy[mas_leave_type_id]">
             <option value="" disabled selected hidden>Select your option</option>
             @foreach($leaves as $leave)
-            <option value="{{ $leave->id }}" {{ old('leave_policy.mas_leave_type_id', isset($leavePolicy) ? $leavePolicy->mas_leave_type_id : '') == $leave->id ? 'selected' : '' }}>{{ $leave->name }}</option>
+            <option value="{{ $leave->id }}" {{  $leavePolicy->mas_leave_type_id == $leave->id ? 'selected' : '' }}>{{ $leave->name }}</option>
             @endforeach
         </select>
     </div>
@@ -19,14 +19,14 @@
 <div class="row">
     <span class="col-sm-4 ">Policy Name <span class="text-danger">*</span> </span>
     <div class="col-sm-4">
-        <input type="text" name="leave_policy[name]" value="{{ old('leave_policy.name',isset($leavePolicy->name)?$leavePolicy->name:'')}}" placeholder="Policy name" class="form-control" required>
+        <input type="text" name="leave_policy[name]" value="{{$leavePolicy->name}}" placeholder="Policy name" class="form-control" required>
     </div>
 </div>
 
 <div class="row">
     <span class="col-sm-4 ">Policy Description </span>
     <div class="col-sm-4">
-        <textarea class="form-control" placeholder="Description" role="3" id="txtDesc" name="leave_policy[description]">{{ old('leave_policy.description',isset($leavePolicy->description)?$leavePolicy->description:'')}}</textarea>
+        <textarea class="form-control" placeholder="Description" role="3" id="txtDesc" name="leave_policy[description]">{{ $leavePolicy->description}}</textarea>
     </div>
 </div>
 
@@ -34,7 +34,7 @@
     <span class="col-sm-4 ">Start Date <span class="text-danger">*</span> </span>
     <div class="col-sm-4">
         <div class="cal-icon">
-            <input type="date" name="leave_policy[start_date]" value="{{old('leave_policy.start_date',isset($leavePolicy->start_date)?$leavePolicy->start_date:'')}}" placeholder="dd-mmm-yyyy" class="form-control mycal hasDatepicker" style="background-color: rgb(255, 255, 255);">
+            <input type="date" name="leave_policy[start_date]" value="{{$leavePolicy->start_date}}" placeholder="dd-mmm-yyyy" class="form-control mycal hasDatepicker" style="background-color: rgb(255, 255, 255);">
         </div>
     </div>
 </div>
@@ -43,7 +43,7 @@
     <span class="col-sm-4 ">End Date </span>
     <div class="col-sm-4">
         <div class="cal-icon">
-            <input type="date" name="leave_policy[end_date]" value="{{old('leave_policy.end_date',isset($leavePolicy->end_date)?$leavePolicy->end_date:'')}}" placeholder="dd-mmm-yyyy" class="form-control mycal" style="background-color: rgb(255, 255, 255);">
+            <input type="date" name="leave_policy[end_date]" value="{{$leavePolicy->end_date}}" placeholder="dd-mmm-yyyy" class="form-control mycal" style="background-color: rgb(255, 255, 255);">
         </div>
     </div>
 </div>
@@ -53,16 +53,16 @@
     <div class="col-sm-4">
         <select class="form-control" id="ddlStatus" name="leave_policy[status]">
             <option value="">Select</option>
-            <option value="0 " {{ old('leave_policy.status',isset($leavePolicy->status)?$leavePolicy->status:'') == 0 ? 'selected' : '' }}>Draft</option>
-            <option value="1" {{ old('leave_policy.status',isset($leavePolicy->status)?$leavePolicy->status:'') == 1 ? 'selected' : '' }}>Enforce</option>
+            <option value="0 " {{ $leavePolicy->status == 0 ? 'selected' : '' }}>Draft</option>
+            <option value="1" {{ $leavePolicy->status == 1 ? 'selected' : '' }}>Enforce</option>
         </select>
     </div>
 </div>
 
 <div class="row">
-    <span class="col-sm-4 "> Is Information Only </span>
+    <span class="col-sm-4"> Is Information Only </span>
     <div class="col-sm-4 ">
         <input type="checkbox" id="chkIsInformationOnly" value="1" name="leave_policy[is_information_only]"
-            {{ old('leave_policy.is_information_only', isset($leavePolicy->is_information_only) ? $leavePolicy->is_information_only : false) ? 'checked' : '' }} />
+            {{ $leavePolicy->is_information_only ? 'checked' : '' }} />
     </div>
 </div>
