4f087e30d59e6b58a4c7a1520f5f2492bd964e96
Phuntsho Dema <pempoo1998@gmail.com>
2024-10-22 12:41:13 +0600

    expense

--- resources/views/expense/apply/types/conveyance.blade.php
+++ resources/views/expense/apply/types/conveyance.blade.php
@@ -4,7 +4,7 @@
           <div class="col-md-4">
               <div class="form-group">
                   <label for="travel_type">Travel Type<span class="text-danger">*</span></label>
-                  <select class="form-control" id="travel_type" name="travel_type" required>
+                  <select class="form-control" id="travel_type" name="travel_type">
                       <option value="" disabled selected hidden>Select your option</option>
                       @foreach(config('global.travel_types') as $key => $label)
                         <option value="{{ $key }}" {{ old('travel_type') == $key ? 'selected' : '' }}>{{ $label }}</option>
@@ -15,7 +15,7 @@
           <div class="col-md-4">
               <div class="form-group">
                   <label for="travel_mode">Mode of Travel<span class="text-danger">*</span></label>
-                  <select class="form-control" id="travel_mode" name="mode_of_travel" required>
+                  <select class="form-control" id="travel_mode" name="mode_of_travel">
                       <option value="" disabled selected hidden>Select your option</option>
                       @foreach(config('global.travel_modes') as $key => $label)
                         <option value="{{ $key }}" {{ old('mode_of_travel') == $key ? 'selected' : '' }}>{{ $label }}</option>
@@ -26,7 +26,7 @@
           <div class="col-md-4">
               <div class="form-group">
                   <label for="travel_from_date">Travel From Date<span class="text-danger">*</span></label>
-                  <input type="date" class="js-datepicker form-control" name="travel_from_date" value="{{ old('travel_from_date') }}" placeholder="dd/mm/yy" required />
+                  <input type="date" class="js-datepicker form-control" name="travel_from_date" value="{{ old('travel_from_date') }}" placeholder="dd/mm/yy" />
               </div>
           </div>
       </div>
@@ -35,19 +35,19 @@
           <div class="col-md-4">
               <div class="form-group">
                   <label for="travel_to_date">Travel to Date<span class="text-danger">*</span></label>
-                  <input type="date" class="js-datepicker form-control" name="travel_to_date" value="{{ old('travel_to_date') }}" placeholder="dd/mm/yy" rerquired />
+                  <input type="date" class="js-datepicker form-control" name="travel_to_date" value="{{ old('travel_to_date') }}" placeholder="dd/mm/yy" />
               </div>
           </div>
           <div class="col-md-4">
               <div class="form-group">
                   <label for="travel_from">Travel From<span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" name="travel_from" value="{{ old('travel_from') }}" required />
+                  <input type="text" class="form-control" name="travel_from" value="{{ old('travel_from') }}" />
               </div>
           </div>
           <div class="col-md-4">
               <div class="form-group">
                   <label for="travel_to">Travel To<span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" name="travel_to" value="{{ old('travel_to') }}" required />
+                  <input type="text" class="form-control" name="travel_to" value="{{ old('travel_to') }}" />
               </div>
           </div>
       </div>
