6be6cf08a8aca976c693a04c948a838e6fd4d512
kingaseldon <kingaseldon36@gmail.com>
2024-11-27 15:38:16 +0600

    asset

--- resources/views/asset/commission/index.blade.php
+++ resources/views/asset/commission/index.blade.php
@@ -1,150 +1,167 @@
 @extends('layouts.app')
 @section('page-title', 'Commission')
 @section('content')
-<style>
-    .col-md-2 {
-        -ms-flex: 0 0 16.666667%;
-        flex: 0 0 16.666667%;
-        max-width: 12.5%;
-    }
 
-    .col-md-1 {
-        padding-top: 25px;
-    }
-</style>    
 <div class="block-header block-header-default">
-    <div class="form-group">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="form-group form-focus select-focus">
-                    <label class="control-label">Commission No</label>
-                    <input type="text" placeholder="" class="form-control" id="comno">
-                </div>
-            </div>
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header"></div>
+            <div class="card-body">
 
-            <div class="col-md-3">
-                <div class="form-group form-focus focused">
-                    <label class="control-label">Commission Date </label>
-                    <input type="date" class="form-control" name="comdate">
-                </div>
-            </div>
+                <div class="row">
 
-            <div class="col-md-3">
-                <div class="form-group form-focus select-focus">
-                    <label class="control-label">GRN</label>
-                    <select class="form-control" name="grn">
-                        <option value="" disabled selected hidden>Select</option>
-                    </select>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="form-group form-focus select-focus">
-                    <label class="control-label">Employee Name</label>
-                    <input type="text" placeholder="Miss Pema" class="form-control" id="empname">
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="form-group form-focus select-focus">
-                    <label class="control-label">Department</label>
-                    <select class="form-control" name="dedpartment">
-                        <option value="" disabled selected hidden>Select</option>
-                    </select>
-                </div>
-            </div>
-
-            <div class="col-4">
-                <label for="attachment">File</label>
-                <input type="file" class="form-control" name="attachment" required="required">
-            </div>
-
-            <div class="row row-sm">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <div id="basic-datatable_wrapper" class="dataTables_wrapper dt-bootstrap5 no-footer">
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <div class="dataTables_length" id="responsive-datatable_length"
-                                                data-select2-id="responsive-datatable_length">
-                                            </div>
-                                            <div class="dataTables_scroll">
-                                                <div class="dataTables_scrollHead"
-                                                    style="overflow: scroll; position: relative; border: 0px; width: 100%;">
-                                                    <div class="dataTables_scrollHeadInner"
-                                                        style="box-sizing: content-box; padding-right: 0px;">
-                                                        <table
-                                                            class="table table-bordered text-nowrap border-bottom dataTable no-footer"
-                                                            id="basic-datatable table-responsive">
-                                                            <thead>
-                                                                <tr role="row">
-                                                                    <th>
-                                                                        PO
-                                                                    </th>
-                                                                    <th>
-                                                                        Asset No
-                                                                    </th>
-                                                                    <th>
-                                                                        Item Description
-                                                                    </th>
-                                                                    <th>
-                                                                        UOM
-                                                                    </th>
-                                                                    <th>
-                                                                        Dzongkhag
-                                                                    </th>
-                                                                    <th>
-                                                                        Quantity
-                                                                    </th>
-                                                                    <th>
-                                                                        Date Placed in Service
-                                                                    </th>
-                                                                    <th>
-                                                                        Site Name
-                                                                    </th>
-                                                                    <th>
-                                                                        Remarks
-                                                                    </th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td></td>
-                                                                    <td>+</td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <br><br>
-                                            <div class="col-sm-12 text-center">
-                                                <button class="btn btn-primary btn-lg text-center" id="btn_create" type="button">Submit</button>
-                                                <input type="hidden" id="hdn_count" value="0">
-                                                <button class="btn btn-primary btn-lg text-center" id="btn_create" type="button">Reset</button>
-                                                <input type="hidden" id="hdn_count" value="0">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="commission">Commission No</label>
+                            <input type="text" class="form-control" name="ommission" value="" disabled>
                         </div>
                     </div>
-                </div>
-            </div>
-        
-        </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="commission_date">Commission Date<span class="text-danger">*</span></label>
+                            <input type="date" class="form-control" name="commission_date" disabled>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="grn">GRN<span class="text-danger">*</span></label>
+                            <select class="form-control" name="grn">
+                                <option value="" disabled selected hidden>Select your option</option>
+                                <option value="">Individual</option>
 
+                            </select>
+                        </div>
+                    </div>
+
+
+
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="employee">Employee Name</label>
+                            <input type="text" class="form-control" name="employee" value="" disabled>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="department">Department</label>
+                            <input type="text" class="form-control" name="department" value="" disabled>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="file">File</label>
+                            <input type="file" class="form-control" name="file" value="" disabled>
+                        </div>
+                    </div>
+
+
+                    <div class="table-responsive">
+                        <table class="table table-condensed table-bordered table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th width="3%" class="text-center">#</th>
+                                    <th>
+                                        PO
+                                    </th>
+                                    <th>
+                                        Item Description
+                                    </th>
+                                    <th>
+                                        UOM
+                                    </th>
+                                    <th>
+                                        Dzongkhag
+                                    </th>
+                                    <th>
+                                        Quantity
+                                    </th>
+                                    <th>
+                                        Date Placed in Service
+                                    </th>
+                                    <th>
+                                        Site Name
+                                    </th>
+                                    <th>
+                                        Remarks
+                                    </th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td class="text-center">
+                                        <a href="" class="delete-table-row btn btn-danger btn-sm"><i class="fa fa-times"></i></a>
+                                    </td>
+                                    <td>
+                                        <select class="form-control form-control-sm resetKeyForNew" name="po">
+                                            <option value="" disabled selected hidden>Select</option>
+                                            <option value="122">1212</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <select class="form-control form-control-sm resetKeyForNew" name="item">
+                                            <option value="" disabled selected hidden>Select</option>
+                                            <option value="122">1212</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" name="UOM" class="form-control form-control-sm resetKeyForNew" disabled>
+                                    </td>
+                                    <td>
+                                        <select class="form-control form-control-sm resetKeyForNew" name="store">
+                                            <option value="" disabled selected hidden>Select</option>
+
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" name="stock_status" class="form-control form-control-sm resetKeyForNew" disabled>
+
+                                    </td>
+                                    <td>
+                                        <input type="number" name="quantity" class="form-control form-control-sm resetKeyForNew">
+
+                                    </td>
+                                    <td>
+                                        <select class="form-control form-control-sm resetKeyForNew" name="dzongkhag">
+                                            <option value="" disabled selected hidden>Select</option>
+
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <select class="form-control form-control-sm resetKeyForNew" name="store">
+                                            <option value="" disabled selected hidden>Select </option>
+                                            <option value="122">1212</option>
+                                        </select>
+                                    </td>
+                                </tr>
+
+                                <tr class="notremovefornew">
+                                    <td colspan="8"></td>
+                                    <td class="text-right">
+                                        <a href="#" class="add-table-row btn btn-sm btn-info" style="font-size: 13px"><i class="fa fa-plus"></i> Add New Row</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div class="card-footer">
+                @include('layouts.includes.buttons', [
+                'buttonName' => 'Submit',
+                'cancelUrl' => url('asset/commission') ,
+                'cancelName' => 'CANCEL'
+                ])
+
+                <input class="btn btn-info" type="reset" value="Reset">
+
+            </div>
+
+        </div>
     </div>
 </div>
 
