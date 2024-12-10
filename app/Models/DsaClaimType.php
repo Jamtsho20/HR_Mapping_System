512f471f80034ad12d6b4f385215097b61babafc
Phuntsho Dema <pempoo1998@gmail.com>
2024-12-10 09:33:02 +0600

    changes

--- app/Models/DsaClaimType.php
+++ app/Models/DsaClaimType.php
@@ -2,18 +2,20 @@
 
 namespace App\Models;
 
-use App\Models\MasApprovalRule;
-use App\Traits\CreatedByTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\CreatedByTrait;
+use App\Models\AssetTransferApplication;
+use App\Models\MasApprovalRule;
 
 class DsaClaimType extends Model
 {
     use HasFactory, CreatedByTrait;
 
-
     public function approvableRule() // relationship with mas_approvable_rules
     {
-        return $this->morphMany(MasApprovalRule::class, 'approvable');//
+        return $this->morphMany(MasApprovalRule::class, 'approvable');
     }
+
+ 
 }
