af92ec7d982cb2cf293a52bb56081db4f0bcc46f
Phuntsho Dema <pempoo1998@gmail.com>
2024-12-19 12:21:03 +0600

    changes in leave

--- app/Models/ApplicationHistory.php
+++ app/Models/ApplicationHistory.php
@@ -10,7 +10,10 @@ class ApplicationHistory extends Model
     use HasFactory;
 
     protected $fillable = [
-        'approval_option', 'hierarchy_id', 'max_level_id', 'next_level_id', 'approver_role_id', 'approver_emp_id', 'level_sequence', 'status', 'remarks', 'action_performed_by', 'sap_response'
+        'approval_option', 'hierarchy_id', 'max_level_id', 'next_level_id', 'approver_role_id', 'approver_emp_id', 'level_sequence', 'status', 'remarks', 'action_performed_by', 'sap_response', 'application_type',  // Polymorphic type
+        'application_id',  //new  
+        'status',            //new
+        'remarks', //new
     ];
 
     public function application()
