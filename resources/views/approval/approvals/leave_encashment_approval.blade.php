774733b980f39894cb1e1b24ac98d971212ade62
kingaseldon <kingaseldon36@gmail.com>
2024-12-30 12:42:12 +0600

    chnages

--- resources/views/approval/approvals/leave_encashment_approval.blade.php
+++ resources/views/approval/approvals/leave_encashment_approval.blade.php
@@ -9,7 +9,8 @@
                             <thead>
                                 <tr role="row" class="thead-light">
                                     <th>
-                                        <input type="checkbox" id="select_all" class="select_all" data-item-class="bulk_checkbox" title="select all">
+                                        <input type="checkbox" id="select_all" class="select_all"
+                                            data-item-class="bulk_checkbox" title="select all">
                                     </th>
                                     <th>
                                         EMPLOYEE ID
@@ -34,49 +35,56 @@
                             </thead>
                             <tbody>
                                 @forelse ($results->get(4) as $leave)
-                                <tr>
-                                    <td><input type="checkbox" class="bulk_checkbox" value="{{ $leave->id }}"></td>
-                                    <td>{{$leave->employee->username}}</td>
-                                    <td>{{$leave->employee->name}}</td>
-                                    <td>{{ $leave->created_at }}</td>
-                                    <td>{{ $leave->encashment_amount }}</td>
-                                    <td class="text-center">
+                                    <tr>
+                                        <td><input type="checkbox" class="bulk_checkbox" value="{{ $leave->id }}">
+                                        </td>
+                                        <td>{{ $leave->employee->username }}</td>
+                                        <td>{{ $leave->employee->name }}</td>
+                                        <td>{{ $leave->created_at }}</td>
+                                        <td>{{ $leave->amount }}</td>
+                                        <td class="text-center">
 
-                                        @php
-                                        $statusClasses = [
-                                        -1 => 'badge bg-danger',
-                                        0 => 'badge bg-warning',
-                                        1 => 'badge bg-primary',
-                                        2 => 'badge bg-success',
-                                        3 => 'badge bg-info',
-                                        ];
-                                        $statusText = config("global.application_status.{$leave->status}", 'Unknown Status');
-                                        $statusClass = config("global.status_classes.{$leave->status}", 'badge bg-secondary');
-                                        @endphp
+                                            @php
+                                                $statusClasses = [
+                                                    -1 => 'badge bg-danger',
+                                                    0 => 'badge bg-warning',
+                                                    1 => 'badge bg-primary',
+                                                    2 => 'badge bg-success',
+                                                    3 => 'badge bg-info',
+                                                ];
+                                                $statusText = config(
+                                                    "global.application_status.{$leave->status}",
+                                                    'Unknown Status',
+                                                );
+                                                $statusClass = config(
+                                                    "global.status_classes.{$leave->status}",
+                                                    'badge bg-secondary',
+                                                );
+                                            @endphp
 
-                                        <span class="{{ $statusClass }}">{{ $statusText }}</span>
+                                            <span class="{{ $statusClass }}">{{ $statusText }}</span>
 
-                                    </td>
-                                    <!-- <td class="text-center">
+                                        </td>
+                                        <!-- <td class="text-center">
                                         @if ($privileges->edit)
-                                        <a href="{{ url('leave/approval/' . $leave->id . '/edit') }}"
+<a href="{{ url('leave/approval/' . $leave->id . '/edit') }}"
                                             class="edit-btn btn btn-sm btn-rounded btn-outline-success"><i
                                                 class="fa fa-edit"></i> EDIT</a>
-                                        @endif
+@endif
                                         @if ($privileges->delete)
-                                        <a href="#"
+<a href="#"
                                             class="delete-btn btn btn-sm btn-rounded btn-outline-danger"
                                             data-url="{{ url('leave/approval/' . $leave->id) }}"><i
                                                 class="fa fa-trash"></i> DELETE</a>
-                                        @endif
+@endif
                                     </td> -->
-                                </tr>
+                                    </tr>
                                 @empty
-                                <tr>
-                                    <td colspan="8" class="text-center text-danger">
-                                        No Encashment Application Found
-                                    </td>
-                                </tr>
+                                    <tr>
+                                        <td colspan="8" class="text-center text-danger">
+                                            No Encashment Application Found
+                                        </td>
+                                    </tr>
                                 @endforelse
                             </tbody>
                         </table>
