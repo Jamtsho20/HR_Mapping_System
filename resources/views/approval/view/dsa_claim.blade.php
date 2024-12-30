6bb0b5052f58574aaeebbd717064b3e1f0ee05c1
kingaseldon <kingaseldon36@gmail.com>
2024-12-30 17:45:30 +0600

    empdetails for dsa approval

--- resources/views/approval/view/dsa_claim.blade.php
+++ resources/views/approval/view/dsa_claim.blade.php
@@ -14,6 +14,8 @@
                     </div>
                 </div>
                 <div class="row">
+                    @include('components.employee-details', ['empDetails' => $empDetails])
+
                     <div class="col-md-12">
                         <table style="width:100%;" class="simple-table">
                             <tbody>
