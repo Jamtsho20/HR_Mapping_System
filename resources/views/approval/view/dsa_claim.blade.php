cbf987da581f5fbcf22cd0dc9b86496350b76174
kingaseldon <kingaseldon36@gmail.com>
2024-12-30 17:47:41 +0600

    empdetails for dsa approval

--- resources/views/approval/view/dsa_claim.blade.php
+++ resources/views/approval/view/dsa_claim.blade.php
@@ -6,16 +6,11 @@
 @endsection
 @section('content')
     <div class="row">
+        @include('components.employee-details', ['empDetails' => $empDetails])
+
         <div class="col-lg-12">
             <div class="col-sm-12 card" style="padding-top: 16px;padding-bottom: 18px;">
                 <div class="row">
-                    <div class="col-md-12">
-                        <h6>DSA Details</h6>
-                    </div>
-                </div>
-                <div class="row">
-                    @include('components.employee-details', ['empDetails' => $empDetails])
-
                     <div class="col-md-12">
                         <table style="width:100%;" class="simple-table">
                             <tbody>
