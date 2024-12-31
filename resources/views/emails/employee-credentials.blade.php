ae86d64f29f7a133b2e7a5d9bf252c211dca623c
kingaseldon <kingaseldon36@gmail.com>
2024-12-31 23:38:26 +0600

    chnage

--- resources/views/emails/employee-credentials.blade.php
+++ resources/views/emails/employee-credentials.blade.php
@@ -4,12 +4,16 @@
 
     Greetings for the day!!
 
-    You have been granted access to the HRMS. Please use the credentials provided below to log into the system.
-    Once logged in, we strongly recommend changing your password immediately for security purposes.
 
-    URL: https://hrms.tashicell.com
-    Username: {{ $employee->username }}
-    Password: {{ $password }}
+    <p>
+        You have been granted access to the HRMS. Please use the credentials provided below to login to the system.
+        Once logged in, we strongly recommend changing your password immediately for security purposes.
+    </p>
+
+
+    <p> URL: https://hrms.tashicell.com</p>
+    <p> Username: {{ $employee->username }}</p>
+    <p> Password: {{ $password }}</p>
 
 
     Thanks,
