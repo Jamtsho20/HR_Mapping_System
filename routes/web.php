8a6c362c90175120f62e94c6ef9c6494ebb058b2
kingaseldon <kingaseldon36@gmail.com>
2024-12-31 22:55:37 +0600

    web

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,17 @@ require __DIR__ . '/auth.php';
 require __DIR__ . '/payroll.php';
 Route::redirect('/', '/login', 301);
 
+$proxy_url    = 'http://hrms.tashicell.com';
+$proxy_schema = 'https';
+
+if (!empty($proxy_url)) {
+    URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+    URL::forceScheme($proxy_schema);
+}
+
 Route::get('/updateemppas', function () {
     // Process employees in chunks
     \DB::table('mas_employees')
