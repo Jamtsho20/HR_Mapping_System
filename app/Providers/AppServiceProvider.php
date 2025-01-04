704e4d0a413e841fa994669d585c66dc6541d0b1
kingaseldon <kingaseldon36@gmail.com>
2025-01-04 19:31:34 +0600

    pushed to live

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
 	// User::observe(UserObserver::class);
-//	if (env('APP_ENV') === 'production') {
-  //          URL::forceRootUrl("https://hrms.tashicell.com");
+	// if (env('APP_ENV') === 'production') {
+    //        URL::forceRootUrl("https://hrms.tashicell.com");
     //    }
         // if ($_SERVER['HTTP_HOST'] === 'hrms-mainfile.tashicell.com') {
         //     header('Location: https://hrms.tashicell.com'.$_SERVER['REQUEST_URI'], true, 301);
