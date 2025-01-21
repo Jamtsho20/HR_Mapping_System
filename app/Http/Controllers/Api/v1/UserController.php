49c74f100e23f10962fab42dbcd564b0a4981f49
Tandin Samphel Norbu <norbutandin413@gmail.com>
2025-01-21 15:06:17 +0600

    Changes made for profile pic upload

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    protected $rules = [
+        'profile_pic' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Allowable formats and max size
+    ];
+
+    protected $messages = [
+       'profile_pic.required' => 'Profile picture is required.',
+    ];
+    public function updateProfilePic(Request $request)
+    {
+        $validator = \Validator::make($request->all(), $this->rules, $this->messages);
+        $user = $request->user();
+        // Check if the user exists
+        if (!$user) {
+            return response()->json(['message' => 'User not found.'], 404);
+        }
+
+        // Handle the new profile picture
+        if ($request->hasFile('profile_pic')) {
+            // Delete existing profile picture if it exists
+            if ($user && $user->profile_pic) {
+                unlink($user->profile_pic); // Deletes the old profile pic from storage
+            }
+            // Upload new profile picture and update the path
+            $profilePic = uploadImageToDirectory($request->profile_pic, 'images/users/');
+            $user->profile_pic = $profilePic;
+            $user->save();
+            return response()->json([
+                'message' => 'Profile picture updated successfully.',
+                'profile_pic_url' => $user->profile_pic,
+            ], 200);
+        }
+
+        return response()->json(['message' => 'No profile picture provided.'], 400);
+    }
+}
