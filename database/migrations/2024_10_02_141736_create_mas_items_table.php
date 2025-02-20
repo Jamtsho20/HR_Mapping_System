26088b3b8a6a761829d2e1a52486a85c541420f5
Tandin Samphel Norbu <norbutandin413@gmail.com>
2025-02-20 16:38:53 +0600

    Changes made to asset api

--- database/migrations/2024_10_02_141736_create_mas_items_table.php
+++ database/migrations/2024_10_02_141736_create_mas_items_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             // $table->foreignId('store_id')->index()->constrained('mas_stores')->cascadeOnUpdate()->restrictOnDelete();
             $table->string('item_no')->unique()->index();
+            $table->string('item_no_old')->unique();
             $table->string('item_description');
             // $table->string('item_type')->index()->nullable();
             $table->string('item_group')->index()->nullable()->comment('consumable & fixed asset');
@@ -22,8 +23,8 @@ return new class extends Migration
             // $table->integer('current_stock')->comment('Total stocks availaible in store or live stock available in the system (CStock + RQty - CQty).');
             // $table->integer('received_quantity')->comment('as soon as if goods is received under same item_no in goods_receipt_note update RQTY');
             // $table->integer('changed_quantity')->comment('initially set changed_quantity to received_quantity and if there is update in RQty add to existing.');
-            $table->boolean('is_fixed_asset')->default(1)->comment('1 => fixed asset, 0 => other type of asset (no need to comission)'); 
-            $table->boolean('status')->comment('1 => active, 0 => inactive and this it self will act as fa_enabled or disabled'); 
+            $table->boolean('is_fixed_asset')->default(1)->comment('1 => fixed asset, 0 => other type of asset (no need to comission)');
+            $table->boolean('status')->comment('1 => active, 0 => inactive and this it self will act as fa_enabled or disabled');
             // $table->timestamp('last_synced_at')->nullable()->comment('Tracks last sync with SAP'); // Tracks last sync with SAP
             $table->foreignId('created_by')->index()->constrained('mas_employees');
             $table->foreignId('updated_by')->index()->nullable()->constrained('mas_employees');
