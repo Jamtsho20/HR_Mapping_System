36bec9b64e4ed666d7537fffc630ef4ff0f7eac2
Heran-foo <heranghalley123@gmail.com>
2025-03-10 12:22:18 +0600

    update on commission_details table

--- database/migrations/2025_02_05_101619_create_asset_commission_details_table.php
+++ database/migrations/2025_02_05_101619_create_asset_commission_details_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->foreignId('commission_id')->index()->constrained('asset_commission_applications')->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('received_detail_serial_id')->index()->constrained('goods_received_detail_serials')->cascadeOnUpdate()->cascadeOnDelete();
             $table->date('date_placed_in_service');
-            $table->string('dzongkhag');
-            $table->string('site_name');
+            $table->foreignId('dzongkhag_id')->index()->nullable()->constrained('mas_dzongkhags');
+            $table->foreignId('office_id')->index()->nullable()->constrained('mas_offices')->comment('for now no need to make use of this only for future purpose');
+            $table->foreignId('site_id')->index()->nullable()->constrained('mas_sites');
             $table->text('remark')->nullable();
-            $table->boolean('status')->comment('1 => commissioned, 0 => Not Commisioned');
             $table->timestamps();
         });
     }
