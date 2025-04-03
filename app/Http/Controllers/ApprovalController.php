0ed3e35d6ba4c477a7b970eca84dc860848ef88f
Phuntsho Dema <pempoo1998@gmail.com>
2025-04-03 16:19:43 +0600

    changes

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -719,7 +719,7 @@ class ApprovalController extends Controller
                 ->whereIn('status', $statuses)
                 ->filter($request, false)
                 ->whereYear('created_at', Carbon::now()->year)
-                ->orderBy('created_at')
+                ->orderBy('created_at', 'desc')
                 ->paginate(config('global.pagination'))
                 ->withQueryString();
         };
