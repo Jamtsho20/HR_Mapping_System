7f1efbbab113e7471a55ae20be65d6f976b43cc3
Tandin Samphel Norbu <norbutandin413@gmail.com>
2025-06-10 09:30:13 +0600

    Chagnes for section error

--- app/Services/ApprovalService.php
+++ app/Services/ApprovalService.php
@@ -16,12 +16,12 @@ class ApprovalService
     {
         $userRoles = auth()->user()->roles()->pluck('role_id')->toArray();
         //checking for section
-        if(!auth()->user()->empJob->mas_section_id){
-            throw ValidationException::withMessages([
-                'section' => 'You have not been assigned to any section, please contact your admin.',
-            ]);
+        // if(!auth()->user()->empJob->mas_section_id){
+        //     throw ValidationException::withMessages([
+        //         'section' => 'You have not been assigned to any section, please contact your admin.',
+        //     ]);
 
-        }
+        // }
 
         // incase if employee has this two roles then it will be different from normal hierarchy
         // Filter and collect the desired roles delegatedRole function defined in helpers.php to make use in other part of the application
