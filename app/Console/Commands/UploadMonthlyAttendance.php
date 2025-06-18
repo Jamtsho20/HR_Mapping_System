3b2379e666df3d25e32035098ce906f9bf21b5fa
Heran-foo <heranghalley123@gmail.com>
2025-06-18 15:57:14 +0600

    update on monthly attendance schedular

--- app/Console/Commands/UploadMonthlyAttendance.php
+++ app/Console/Commands/UploadMonthlyAttendance.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\DailyAttendance;
 use App\Models\EmployeeAttendance;
+use App\Models\MasDepartment;
 use App\Models\MasSection;
 use Illuminate\Console\Command;
 use Carbon\Carbon;
@@ -42,15 +43,15 @@ class UploadMonthlyAttendance extends Command
                 'created_at' => now()
             ]);
 
-            $sections = MasSection::whereStatus(1)->get();
+            $departments = MasDepartment::whereStatus(1)->get();
             $insertData = [];
             // create day wise attendance for each section for each days in a current month.
-            foreach($sections as $section){
+            foreach($departments as $department){
                 for($day = 1; $day <= $daysInMonth; $day++){
                     $insertData[] = [
                         'attendance_id' => $monthlyAttendance->id,
-                        'department_id' => $section->mas_department_id,
-                        'section_id' => $section->id,
+                        'department_id' => $department->id,
+                        'section_id' => null,
                         'day' => $day,
                         'status' => 1,
                         'created_by' => 1,
--- app/Http/Controllers/Api/Attendance/AttendanceApiController.php
+++ app/Http/Controllers/Api/Attendance/AttendanceApiController.php
@@ -32,12 +32,12 @@ class AttendanceApiController extends Controller
         $user = auth()->user();
         $attendanceFeatures = MasAttendanceFeature::whereStatus(1)->get(['id', 'name', 'is_mandatory']);
         $attendanceService = new AttendanceService();
-        $officeTiming = $attendanceService->getEffectiveOfficeTiming($user);
+        $officeTiming = $attendanceService->getEffectiveOfficeTiming($user) ?? [];
         $attendanceEntry = $attendanceService->empAttendanceEntry($user) ?? [];
         $attendanceStatuses = AttendanceStatus::get(['id', 'code', 'description']);
-        if(!$officeTiming){
-            return $this->errorResponse('Something went wrong while fetching effective office timing and geo location. Please try again or ask system admin for further information.');
-        }
+        // if(!$officeTiming){
+        //     return $this->errorResponse('Something went wrong while fetching effective office timing and geo location. Please try again or ask system admin for further information.');
+        // }
 
         // if(!$attendanceEntry){
         //     return $this->errorResponse('Attendance entry for date ' . now() . ' has not been created. Please try again or ask system admin for further information.');
