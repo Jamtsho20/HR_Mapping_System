fd26d4a8fd8465ba43330749b38e46d613d3f326
kingaseldon <kingaseldon36@gmail.com>
2025-07-31 15:41:46 +0600

    chnage

--- app/Http/Controllers/Reports/TaxScheduleReportController.php
+++ app/Http/Controllers/Reports/TaxScheduleReportController.php
@@ -106,7 +106,7 @@ class TaxScheduleReportController extends Controller
             return $salary->details['net_pay'] ?? 0;
         });
         $totalPF = $taxSchedules->sum(function ($salary) {
-            return $salary->details['deductions']['pf'] ?? 0;
+            return $salary->details['deductions']['PF Contr'] ?? 0;
         });
 
         $totalGross = $taxSchedules->sum(function ($salary) {
