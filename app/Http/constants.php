0e2f523b35699d919290d9412edcc5af4218934d
Heran-foo <heranghalley123@gmail.com>
2025-08-11 14:07:25 +0600

    update

--- app/Http/constants.php
+++ app/Http/constants.php
@@ -25,7 +25,7 @@ const MANAGING_DIRECTOR = 8;
 const ADMIN = 1;
 const HR = 3;
 const HR_MANAGER = 11;
-const ATTENDANCE_MANAGER = 16;
+const ATTENDANCE_MANAGER = 19;
 const SIFA_MANAGER = 17;
 
 //approval head constant from mas_approval_heads table
