40d256e088800e735e299dd8c31d3b2bb80f38e6
Heran-foo <heranghalley123@gmail.com>
2025-10-22 10:07:54 +0600

    updated after checking the INFORMED_LATE_STATUS and updated remark as well if remarks is not provided by supervisor

--- app/Http/Controllers/Api/Attendance/AttendanceApiController.php
+++ app/Http/Controllers/Api/Attendance/AttendanceApiController.php
@@ -153,10 +153,10 @@ class AttendanceApiController extends Controller
                     $attendanceStatus = ABSENT_STATUS;
                     $diff = $checkInTime->diff($startTime);
                     $remarks = "Reported late by " . implode(' ', $this->splitTime($diff)) . ", thus marked absent (System generated).";
-                } elseif ($request->check_type == 'check-in' && $request->check_in_at && $bufferedTime->lessThan($checkInTime)) {
-                    $attendanceStatus = LATE_STATUS;
+                } elseif ($request->check_type == 'check-in' && $request->check_in_at  && $bufferedTime->lessThan($checkInTime)) {
+                    $attendanceStatus = $attendance->attendance_status_id != INFORMED_LATE_STATUS ? LATE_STATUS : INFORMED_LATE_STATUS;
                     $diff = $checkInTime->diff($bufferedTime);
-                    $remarks = "Reported late by " . implode(' ', $this->splitTime($diff)) . " (System generated).";
+                    $remarks = $attendance->attendance_status_id != INFORMED_LATE_STATUS ? "Reported late by " . implode(' ', $this->splitTime($diff)) . " (System generated)." : ($attendance->remarks ?? 'Marked as Informed late by supervisor (System generated).');
                 } else {
                     $attendanceStatus = (($request->check_type == 'check-in' && $request->check_in_at) || ($request->check_type == 'check-out' && $request->check_out_at)) ? PRESENT_STATUS : $attendanceStatus;
                 }
